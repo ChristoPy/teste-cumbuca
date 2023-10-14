6bdd7293b0e6a4457c3092653576544985bc4525
Christopher Ribeiro <christo_campiglia@hotmail.com>
2023-10-14 19:45:14 +0000

    Update user model to have taxId

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,10 +1,15 @@
 import { Schema, ObjectId } from "mongoose";
 import db from "../database";
 
+export interface TaxId {
+  type: 'CPF' | 'CNPJ'
+}
+
 export interface User {
   _id: ObjectId;
   name: string;
   email: string;
+  taxId: string;
   password: string;
   createdAt: number;
   updatedAt: number;
@@ -14,12 +19,14 @@ export interface PublicUser {
   _id: string;
   name: string;
   email: string;
+  taxId: TaxId;
 }
 
 export const UserSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  taxId: { type: String, required: true },
   createdAt: { type: Number, default: Date.now },
   updatedAt: { type: Number, default: Date.now },
 });
