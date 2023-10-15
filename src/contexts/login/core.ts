5efc4d5d733556035d7dbe6858cafb40287c6ac5
Christopher Ribeiro <christo_campiglia@hotmail.com>
2023-10-15 20:31:37 +0000

    Return user wallet at login

--- src/contexts/login/core.ts
+++ src/contexts/login/core.ts
@@ -3,6 +3,7 @@ import { sign } from 'jsonwebtoken'
 import { TaxId, UserModel } from "../../models/user";
 import { LoginInput, LoginResult } from "./types";
 import { Result } from '../../models/result';
+import { WalletModel } from '../../models/wallet';
 
 export async function login(data: LoginInput): Promise<Result<LoginResult>> {
   const user = await UserModel.findOne({ email: data.email });
@@ -13,6 +14,11 @@ export async function login(data: LoginInput): Promise<Result<LoginResult>> {
   const match = compareSync(data.password, user.password);
   if (!match) {
     return { error: 'E-mail or password is incorrect' }
+  }
+
+  const wallet = await WalletModel.findOne({ owner: user._id })
+  if (!wallet) {
+    return { error: 'Could not load user wallet' }
   }
 
   const token = sign({ _id: user._id }, process.env.JWT_SECRET as string, { expiresIn: '1d' });
@@ -30,6 +36,10 @@ export async function login(data: LoginInput): Promise<Result<LoginResult>> {
         name: user.name,
         email: user.email,
         taxId,
+      },
+      wallet: {
+        _id: wallet.id,
+        amount: wallet.id
       }
     },
   }
