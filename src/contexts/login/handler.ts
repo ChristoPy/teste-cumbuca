663dd894ed4fb7b6f34929a99aad499ed8c1a9ea
Christopher Ribeiro <christo_campiglia@hotmail.com>
2023-10-15 20:39:06 +0000

    Update cached info
    Reference the user in the token
    Reference the wallet in the token

--- src/contexts/login/handler.ts
+++ src/contexts/login/handler.ts
@@ -12,7 +12,7 @@ async function handler(fastify: FastifyInstance, request: FastifyRequest, reply:
 
   fastify.redis.set(`user:${data!.user._id}`, JSON.stringify(data!.user));
   fastify.redis.set(`wallet:${data!.wallet._id}`, JSON.stringify(data!.wallet));
-  fastify.redis.set(`token:${data!.token}`, JSON.stringify(data!.user._id), 'EX', 86400);
+  fastify.redis.set(`token:${data!.token}`, JSON.stringify({ user: data!.user._id, wallet: data!.wallet._id }), 'EX', 86400);
   reply.status(200).send(data);
 }
 
