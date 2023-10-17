cef29b03e6bf7659fb62791112fbd17ddbe01f6f
Christopher Ribeiro <christo_campiglia@hotmail.com>
2023-10-17 14:13:41 +0000

    Add initial queue processing
    Load transaction
    Load wallet
    Handle deposit and withdraw
    Save the data

--- src/queues/transactions.ts
+++ src/queues/transactions.ts
@@ -1,15 +1,33 @@
-import { TransactionStatus, TransactionType } from "../models/transaction"
+import { TransactionModel } from "../models/transaction"
+import { WalletModel } from "../models/wallet"
 import { ProcessorInjectedContext } from "../plugins/queues"
 
 export interface TransactionsJob {
   _id: string
-  amount: number
-  owner: string
-  wallet: string
-  type: TransactionType
-  status: TransactionStatus
 }
 
 export default async function ({ job, fastify }: ProcessorInjectedContext<TransactionsJob>) {
-  console.log(job.data)
+  const transaction = await TransactionModel.findOne({ _id: job.data._id })
+  if (!transaction) {
+    throw new Error('Transaction not found for transaction ID: ' + job.data._id)
+  }
+  const wallet = await WalletModel.findOne({ _id: transaction.wallet })
+  if (!wallet) {
+    transaction.status = 'failed'
+    transaction.save()
+
+    throw new Error('Wallet not found for transaction ID: ' + transaction.wallet)
+  }
+
+  if (transaction.type === 'deposit') {
+    wallet.amount += transaction.amount
+  }
+  if (transaction.type === 'withdraw') {
+    wallet.amount -= transaction.amount
+  }
+
+  transaction.status = 'done'
+
+  wallet.save()
+  transaction.save()
 }
