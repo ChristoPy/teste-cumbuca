a54ac1f1d3b5654ed21f44c35e77e3102fef14ed
Christopher Ribeiro <christo_campiglia@hotmail.com>
2023-10-18 15:38:07 +0000

    Add withdraw rules
    Prevent if insufficient funds

--- src/contexts/withdraw/core.ts
+++ src/contexts/withdraw/core.ts
@@ -2,8 +2,24 @@ import { WithdrawInput, WithdrawResult } from "./types";
 import { TransactionModel } from "../../models/transaction";
 import { FastifyInstance } from "fastify";
 import { Result } from "../../models/result";
+import { WalletModel } from "../../models/wallet";
 
 export async function withdraw(data: WithdrawInput, context: FastifyInstance): Promise<Result<WithdrawResult>> {
+  const wallet = await WalletModel.findOne({ _id: data.wallet })
+  if (!wallet) {
+    return {
+      error: 'Could not find wallet'
+    }
+  }
+
+  console.log(data.amount, wallet.amount, data.amount > wallet.amount)
+
+  if (data.amount > wallet.amount) {
+    return {
+      error: 'Insuficient funds'
+    }
+  }
+
   const transaction = await TransactionModel.create({
     amount: data.amount,
     owner: data.owner,
